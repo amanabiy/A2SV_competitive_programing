673de50e56b8e7af10d5353a32081e3a2c52ac98
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-03 19:15:52 +0300

    better implementation

--- Week-1/Day-2/smallerNumbers.py
+++ Week-1/Day-2/smallerNumbers.py
@@ -1,11 +1,20 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        """ find the smaller numbers from the given index """
-        smaller = []
-        for i in range(len(nums)):
-            count: int = 0
-            for j in range(len(nums)):
-                if (nums[i] > nums[j]):
-                    count += 1
-            smaller.append(count)
-        return smaller
+        """ better solution """
+        number = sorted(nums)
+        less_num = {}
+        for index, num in enumerate(number):
+            if num not in less_num:
+                less_num[num] = index
+        return [less_num[num] for num in nums]
+# class Solution:
+#     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+#         """ find the smaller numbers from the given index """
+#         smaller = []
+#         for i in range(len(nums)):
+#             count: int = 0
+#             for j in range(len(nums)):
+#                 if (nums[i] > nums[j]):
+#                     count += 1
+#             smaller.append(count)
+#         return smaller
