a9fc726290fec138cf336b11421398d04db501ad
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-01-31 16:31:01 +0300

    Create power_of_three.py

---
+++ recurssion/power_of_three.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        """
+        Time: O(n)
+        Space: O(1)
+        """
+        if n == 1:
+            return True
+        if n < 3:
+            return False
+        return self.isPowerOfThree(n/3)
+            
