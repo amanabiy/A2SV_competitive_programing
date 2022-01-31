10d4ff431dfcbdc40274f1b664a2d2dd0a2c7d32
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-01-31 14:25:32 +0300

    Create Power_of_number.py

---
+++ recurssion/Power_of_number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n < 0:
+            return  1/(self.myPow(x, abs(n)))
+        if n == 0:
+            return 1
+        if n % 2 == 0:
+            val = self.myPow(x, n/2)
+            return val * val
+        return x * self.myPow(x, n-1)
+        
