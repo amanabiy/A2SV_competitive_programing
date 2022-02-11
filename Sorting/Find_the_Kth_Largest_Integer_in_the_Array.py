43bdb5c3af7b5cd3562ad340ce7fe6f23c829fef
Amanuel Sisay <amanuelabiy.as@gmail.com>
2022-02-11 22:17:14 +0300

    kth largest integer

--- Sorting/Find_the_Kth_Largest_Integer_in_the_Array.py
+++ Sorting/Find_the_Kth_Largest_Integer_in_the_Array.py
@@ -1,4 +1,15 @@
 class Solution:
     def kthLargestNumber(self, nums: List[str], k: int) -> str:
-        nums.sort(key=lambda x: int(x))
+        """ Numbers with longer length are larger and
+        if they have equal length compare the digits
+        Time: O(nlogn)
+        Space: O(n)
+        """
+        nums.sort(key=lambda num: [len(num), num])
         return nums[-k]
+
+
+# class Solution:
+#     def kthLargestNumber(self, nums: List[str], k: int) -> str:
+#         nums.sort(key=lambda x: int(x))
+#         return nums[-k]
