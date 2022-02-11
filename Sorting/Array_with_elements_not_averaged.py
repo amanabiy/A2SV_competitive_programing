6991bc0dcb048059ed6e71396204a65d6706b502
Amanuel Sisay <amanuelabiy.as@gmail.com>
2022-02-11 21:59:09 +0300

    array with elements not equalt to the average

--- Sorting/Array_with_elements_not_averaged.py
+++ Sorting/Array_with_elements_not_averaged.py
@@ -18,3 +18,12 @@ class Solution:
         if (len(nums) % 2==1):
             ans[even] = nums[len(nums) - 1]
         return ans
+
+
+# could have been implemented better like this
+# class Solution:
+#     def rearrangeArray(self, A: List[int]) -> List[int]:
+#         A.sort()
+#         for i in range(1, len(A), 2):
+#             A[i], A[i - 1] = A[i - 1], A[i]
+#         return A 
