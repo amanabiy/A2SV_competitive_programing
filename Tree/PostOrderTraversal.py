2f20d792e1f6635b1a455dbdce58a1c764502e28
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-03-01 14:23:04 +0300

    Update PostOrderTraversal.py

--- Tree/PostOrderTraversal.py
+++ Tree/PostOrderTraversal.py
@@ -4,6 +4,51 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# Cleaner code
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        stack = [root]
+        ans = []
+        visited = set()
+        visited_left = set()
+        temp = root
+        
+        while stack:
+            if  stack[-1].left and stack[-1].left not in visited:
+                stack.append(stack[-1].left)
+            elif  stack[-1].right and stack[-1].right not in visited:
+                stack.append(stack[-1].right)
+            else:
+                node = stack.pop()
+                visited.add(node)
+                ans.append(node.val)
+                    
+        return ans
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         stack = [root]
