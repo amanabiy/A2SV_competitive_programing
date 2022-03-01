2d5d8c8c5e16bda69fbf697586f03d576a533388
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-03-01 11:32:32 +0300

    using iterative

--- Tree/InOrder_traversal.py
+++ Tree/InOrder_traversal.py
@@ -6,6 +6,43 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = [root]
+        ans = []
+        visited = set()
+        temp = root
+
+        while stack:
+            if stack[-1] == None:
+                stack.pop()
+            elif  stack[-1] not in visited:
+                visited.add(stack[-1])
+                stack.append(stack[-1].left)
+            else:
+                node = stack.pop()
+                ans.append(node.val)
+                if node.right:
+                    stack.append(node.right)
+
+        return ans
+
+
+
+
+
+
+
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         ans = []
         
         def traverse(node):
