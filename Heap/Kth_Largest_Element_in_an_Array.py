c0f0c924dc0e5372f552dacda25a6f7963e75540
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-03-04 10:02:28 +0300

    Create Kth_Largest_Element_in_an_Array.py

---
+++ Heap/Kth_Largest_Element_in_an_Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        """
+        Time: O(nlog(k))
+        Space: O(k)
+        """
+        ans = []
+        for val in nums:
+            if len(ans) < k:
+                heapq.heappush(ans, val)
+            else:
+                if ans[0] < val:
+                    heapq.heappop(ans)
+                    heapq.heappush(ans, val)
+        return ans[0]
